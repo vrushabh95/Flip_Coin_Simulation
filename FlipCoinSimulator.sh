c4b00aa5ff16256952c2c03bcd6219dba8827930
vrushabh95 <mundhe.vrushabh@gmail.com>
2020-12-15 17:56:18 +0530

    Showing The Winner

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -2,6 +2,8 @@
 echo "Welcome to Flip coin Simulation"
 
 
+#uc2
+
 count=1
 HEADS=0
 TAILS=0
@@ -21,3 +23,33 @@ done
 echo "FLIPCOUNT-"$count
 echo "HEADS-"$HEADS
 echo "TAILS-"$TAILS
+
+
+#UC3
+
+count=0
+HEADS=0
+TAILS=0
+
+while [[ $HEADS -lt 21 && $TAILS -lt 21 ]]
+do
+   flip=$((RANDOM%2))
+
+   if [ $flip -eq 1 ]
+   then
+      (( HEADS++ ))
+   else
+      (( TAILS++ ))
+   fi
+   ((count++))
+done
+echo "FLIPCOUNT-"$count
+echo "HEADS $HEADS times"
+echo "TAILS $TAILS times"
+
+if [ $HEADS -gt $TAILS ]
+then
+    echo " HEADS WIN"
+else
+    echo "TAILS WIN"
+fi
